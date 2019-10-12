d407d5c7e2fe8cb6c8086f6c83451cdace9fe232
phutureproof <dale.phutureproof@gmail.com>
2019-10-12 15:55:35 +0100

    adding files

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
 
-
-
+// composer autoloader
 require_once("./vendor/autoload.php");
 
+// imports
 use PhutureProof\Database\Drivers\Sqlite as DB;
 use PhutureProof\Display\Drivers\Terminal as Display;
 
+// storage
 $db = DB::getInstance();
 
+// fetch todos as objects from sqlite storage
 $todos = $db->runQuery("SELECT * FROM todos");
 
+// output a list of html based articles
 Display::Render("<ul>");
 foreach($todos as $todo) {
     Display::Render("\t<li>{$todo->name}</li>");
